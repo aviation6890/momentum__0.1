92d16f3f484dd597c9ecd43af18b7551a568ba53
aviation6890 <aviation6890@gmail.com>
2019-09-24 17:45:29 +0900

    #1.8 Data Types on JS

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+let a = 221;
+const b = a - 5;
+a = 4;
+
+console.log(a, b);
+
 //1.create variable.
 //2.Initialize variable.
 //3.use variable.
 
-let a = 221;
-let b = a - 5;
+/*#1.6
+const means constant.
+상수 즉 변하지 않는수 라는 것임.
+변수의 개념이랑 다름.
+변수가 바뀌어도 된다고  let 변수의 값이 고정적이라면 const
+*/
 
-a = 4;
+/*#1.8
+변수 선언할때 되도록이면 const .
+진짜 필요할때만 let쓸것.
 
-console.log(a, b);
+//String
+const what = "Nicolas";
+console.log(what);
+
+//Boolean is not TEXT!!.
+const wat = true;
+
+//Number
+const wat = 66;
+
+//Float
+const wat = 55.5;
+
+*/
