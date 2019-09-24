3ab0b8a6a2c417124e39290ab090d6650a808591
aviation6890 <aviation6890@gmail.com>
2019-09-24 17:04:01 +0900

    #1.6 Your first JS Variable

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log("Im working. Im JS. Im Beautiful. Im worth it.");
+//1.create variable.
+//2.Initialize variable.
+//3.use variable.
 
-console.log("222");
+let a = 221;
+let b = a - 5;
+
+a = 4;
+
+console.log(a, b);
