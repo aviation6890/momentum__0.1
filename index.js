df52dc0880ee548613ebca44a7f8eadd5fe4295b
aviation6890 <aviation6890@gmail.com>
2019-09-25 10:10:12 +0900

    #2.1.1 More Function Fun

--- index.js
+++ index.js
@@ -1,92 +1,51 @@
-function sayHello(name , age) {
-   console.log("Hello!", name," you have ", age," years of age.");
-}
+const calculator = {
+  plus: function(a, b) {
+    return a + b;
+  },
+  div: function(a, b) {
+    return a / b;
+  },
+  multi: function(a, b) {
+    return a * b;
+  }
+};
 
-sayHello("Nicolas", 15);
+const plus = calculator.plus(5, 5);
+const div = calculator.div(5, 5);
+const multi = calculator.multi(4, 4);
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(plus, div, multi);
 
 //1.create variable.
 //2.Initialize variable.
-//3.use variable.
-
-/*#1.6
-const means constant.
-상수 즉 변하지 않는수 라는 것임.
-변수의 개념이랑 다름.
-변수가 바뀌어도 된다고  let 변수의 값이 고정적이라면 const
-*/
-
-/*#1.8
-변수 선언할때 되도록이면 const .
-진짜 필요할때만 let쓸것.
-
-//String
-const what = "Nicolas";
-console.log(what);
-
-//Boolean is not TEXT!!.
-const wat = true;
-
-//Number
-const wat = 66;
-
-//Float
-const wat = 55.5;
-
-*/
-
-/*#1.9 Organizing Data with Arrays.
-const dayOfWeek = ""; //camel case.
-
-const monday = "mon";
-const the = "tue";
-const wed = "wed";
-const thu ="thu";
-const fri ="fri";
----비효율적임 그래서 나온게 arrays
-
-const dayOfWeek = ['mon','tue','Wed','Thu','Fri',"Sat","Sun"];
-console.log(dayOfWeek);
-*/
-
 /*#1.10 */
 /*
-const dayOfWeek = ["Nicolas", "55", true,"Seoul"];
+ const dayOfWeek = ["Nicolas", "55", true,"Seoul"];
+ 
+ const nicoInfo ={
+     name : "Nico",
+     age :  33,
+     gender : "Male",
+     isHandsome : true,
+     favMovies : ["Along the gods","LOTR","Oldboy"],
+     favFood : [
+         {
+             name:"Kimchi",
+             fatty:false
+         },
+         {
+             name:"Cheese burger",
+             fatty:true
+         }
+     ]
+ };
+ 
+ console.log(nicoInfo);
+ console.log(nicoInfo.gender);
+ console.log(nicoInfo.gender = "Female");
+ console.log(nicoInfo.gender);
+ console.log(console);
+ */
 
-const nicoInfo ={
-    name : "Nico",
-    age :  33,
-    gender : "Male",
-    isHandsome : true,
-    favMovies : ["Along the gods","LOTR","Oldboy"],
-    favFood : [
-        {
-            name:"Kimchi",
-            fatty:false
-        },
-        {
-            name:"Cheese burger",
-            fatty:true
-        }
-    ]
-};
-
-console.log(nicoInfo);
-console.log(nicoInfo.gender);
-console.log(nicoInfo.gender = "Female");
-console.log(nicoInfo.gender);
-console.log(console);
-*/
-
+//const dayOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+//console.log(dayOfWeek);
