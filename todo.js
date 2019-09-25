2a72cf3a0f99783093cadebdf5f9f6528d4d896b
BUSITDOL-MASTER <aviation6890@gmail.com>
2019-09-26 00:09:01 +0900

    #3.5 Making a To Do List part One

--- todo.js
+++ todo.js
@@ -4,13 +4,21 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 function paintToDo(text) {
-  console.log(text);
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  todoList.appendChild(li);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const currentvalue = toDoInput.value;
   paintToDo(currentvalue);
+  toDoInput.value="";
 }
 
 function loadToDo() {
